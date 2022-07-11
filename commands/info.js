6a9aad4cec6a6f8eee1232653b0466afc79fdadb
luzede <85195923+luzede@users.noreply.github.com>
2022-07-11 15:42:13 +0300

    staged info and ping

--- commands/info.js
+++ commands/info.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,10 +8,24 @@ module.exports = {
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('user')
-				.setDescription('Info about a useer')
+				.setDescription('Info about a user')
 				.addUserOption(option => option.setName('target').setDescription('The user')))
 		.addSubcommand(subcommand =>
 			subcommand
 				.setName('server')
 				.setDescription('Info about a server')),
+	async execute(interaction) {
+		const subcommand = interaction.options.getSubcommand();
+
+		if (subcommand === 'user') {
+			const user = interaction.options.getUser('target');
+			await interaction.reply(`Username: ${user.username}\nID: ${user.id}`);
+
+		}
+		else if (subcommand === 'server') {
+			const server = interaction.channel.guild;
+			await interaction.reply(`Server Name: ${server.name}\nMember Count: ${server.memberCount}`);
+		}
+		await wait(10000).then(() => interaction.deleteReply());
+	},
 };
--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.deferReply({ ephemeral: true });
+		// await interaction.reply({ content:'Pong!', ephemeral: true });
+		await wait(5000);
+		await interaction.editReply({ content: 'Pong again!', ephemeral: true });
+		await interaction.followUp({ content: 'Pong again haha!', ephemeral: true });
 	},
 };
