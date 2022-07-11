8b777c67e0e21aeb2d1380ccdba3fda04f978bdd
luzede <85195923+luzede@users.noreply.github.com>
2022-07-11 16:12:41 +0300

    Forgot to stage count.js so I did now

--- commands/count.js
+++ commands/count.js
@@ -3,14 +3,20 @@ const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('count5')
-		.setDescription('Counts to 5!'),
+		.setName('count')
+		.setDescription('Counts to a number!')
+		.addIntegerOption(option => option
+			.setName('number')
+			.setDescription('The number to count to')),
 	async execute(interaction) {
+		const number = interaction.options.addIntegerOption('number');
 		await interaction.reply('0');
-		for (let i = 1; i <= 5; i++) {
+		for (let i = 1; i <= number; i++) {
 			await wait(1000);
 			await interaction.editReply(`${i}`);
 		}
+		await wait(1000);
+		await interaction.editReply(`Done! I counted to ${number}`);
 		await interaction.deleteReply();
 	},
 	global: true,
