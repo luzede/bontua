ca46300078881ed0fd1122767898620460f7f194
luzede <85195923+luzede@users.noreply.github.com>
2022-07-13 20:37:35 +0300

    Tested collector.on to collect interactions on messages

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 // const wait = require('node:timers/promises').setTimeout;
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+// const { MessageActionRow, MessageSelectMenu, MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,6 +14,8 @@ module.exports = {
 	// 	await interaction.editReply({ content: 'Pong again!', ephemeral: true });
 	// 	await interaction.followUp({ content: 'Pong again haha!', ephemeral: true });
 	// },
+
+
 	async execute(interaction) {
 		const row = new MessageActionRow()
 			.addComponents(
@@ -20,7 +23,7 @@ module.exports = {
 					.setCustomId('primary')
 					.setLabel('Primary')
 					.setStyle('PRIMARY')
-					.setDisabled(true)
+					.setDisabled(false)
 					.setEmoji('🔥'),
 			);
 		const embed = new MessageEmbed()
@@ -29,5 +32,42 @@ module.exports = {
 			.setURL('https://discord.js.org')
 			.setDescription('Some description here');
 		await interaction.reply({ content: 'Pong!', components: [row], embeds: [embed] });
+
+		const filter = i => i.customId === 'primary' && i.user.id === interaction.user.id;
+		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		collector.on('collect', async i => {
+			await i.update({ content: 'A button was clicked', components: [], embeds: [] });
+		});
+
+		collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 	},
+
+	// async execute(interaction) {
+	// 	const row = new MessageActionRow()
+	// 		.addComponents(
+	// 			new MessageSelectMenu()
+	// 				.setCustomId('select')
+	// 				.setPlaceholder('Nothing selected')
+	// 				.addOptions([
+	// 					{
+	// 						label: 'Select me',
+	// 						description: 'This is a description',
+	// 						value: 'first_option',
+	// 					},
+	// 					{
+	// 						label: 'You can select me too',
+	// 						description: 'This is also a description',
+	// 						value: 'second_option',
+	// 					},
+	// 				]),
+	// 		);
+
+	// 	const embed = new MessageEmbed()
+	// 		.setColor('#0099ff')
+	// 		.setTitle('Some title')
+	// 		.setURL('https://discord.js.org')
+	// 		.setDescription('Some description here');
+	// 	await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
+	// },
 };
--- events/button.js
+++ events/button.js
@@ -4,6 +4,14 @@ module.exports = {
 		if (!interaction.isButton()) {
 			return;
 		}
-		console.log(interaction);
+		// console.log(interaction);
+		// const filter = i => i.customId === 'primary' && i.user.id === interaction.user.id;
+		// const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		// collector.on('collect', async i => {
+		// 	await i.update({ content: 'A button was clicked', components: [], embeds: [] });
+		// });
+
+		// collector.on('end', async collected => {console.log(`Collected ${collected.size} items`);});
 	},
 };
